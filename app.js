59bd1f0ea8b23e7c3a100396f008159bef279b30
Luiz Henrique E. N. Santos <luizhenrique.egydio@gmailcom>
2025-05-13 11:17:21 -0300

    imagens reais adicionadas

--- app.js
+++ app.js
@@ -6,31 +6,35 @@ const input = document.getElementById('search')
 const modalTitle = document.getElementById('modalTitleId')
 
 
-input.addEventListener("input", ()=>{
+input.addEventListener("input", () => {
     for (let j = 0; j < body.childElementCount; j++) {
         body.firstChild.remove()
     }
 
     const container = document.createElement('div')
     container.className = "container"
-    let arvoresSelecionadas = arvores.filter(a => a.nome.toLowerCase().startsWith(input.value.toLocaleLowerCase()) )
+    let arvoresSelecionadas = arvores.filter(a => a.nome.toLowerCase().includes(input.value.toLocaleLowerCase()))
 
-    if(input.value === '') return
+    if (input.value === '') return
     for (let i = 0; i < arvoresSelecionadas.length; i++) {
         const imgCard = document.createElement('img')
         const imgCardReal = document.createElement('img')
+
         imgCard.src = arvoresSelecionadas[i].card
-        imgCard.className = 'object-fit-contains m-4'
+        imgCard.className = 'object-fit-contains m-2'
         imgCard.alt = `${arvoresSelecionadas[i].nome.toLocaleLowerCase()}`
         imgCard.height = '640'
         imgCard.width = '500'
+        container.append(imgCard)
+
+        if (arvoresSelecionadas[i].imgReal === '') continue
         imgCardReal.src = arvoresSelecionadas[i].imgReal
-        imgCardReal.className = 'object-fit-cover'
+        imgCardReal.className = 'object-fit-cover m-2'
         imgCardReal.alt = `${arvoresSelecionadas[i].nome.toLocaleLowerCase()}`
         imgCardReal.height = '640'
         imgCardReal.width = '500'
-        container.append(imgCard)   
-        container.append(imgCardReal)   
+        container.append(imgCardReal)
+
     }
     body.append(container)
 })
@@ -39,114 +43,114 @@ input.addEventListener("input", ()=>{
 const arvores = [{
     id: 1,
     nome: 'Açoita-Cavalo-Miúdo',
-    card: path +'açoita-cavalo-miudo.png',
+    card: path + 'açoita-cavalo-miudo.png',
     imgReal: pathArvoreReal + 'açoita-cavalo-miudo.jpg'
 }, {
     id: 2,
     nome: 'Andiroba',
     card: path + 'andiroba.png',
     imgReal: pathArvoreReal + 'andiroba.jpg'
-},{
+}, {
     id: 3,
     nome: 'Angico-Branco',
     card: path + 'angico-branco' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'angico_branco.jpg'
+}, {
     id: 4,
     nome: 'Araucária',
     card: path + 'araucaria' + '.png',
-    imgReal: ''
+    imgReal: pathArvoreReal + 'araucária.jpg'
 },
 {
     id: 5,
     nome: 'Babosa-Branca',
     card: path + 'babosa-branca' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'babosa-branca.jpg'
+}, {
     id: 6,
-    nome: 'Cambuca',
+    nome: 'Cambucá',
     card: path + 'cambuca' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'cambucá.png'
+}, {
     id: 7,
     nome: 'Canela-Louro',
     card: path + 'canela-louro' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'canela-louro.jpg'
+}, {
     id: 8,
     nome: 'Cerejeira',
     card: path + 'cerejeira' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'amburana_cearensis.jpg'
+}, {
     id: 9,
     nome: 'Garapeira',
     card: path + 'garapeira' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'garapeira.jpg'
+}, {
     id: 10,
     nome: 'Grumixama',
     card: path + 'grumixama' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'grumixama.jpg'
+}, {
     id: 11,
     nome: 'Guabiroba',
     card: path + 'guabiroba' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'guabiroba.jpg'
+}, {
     id: 12,
     nome: 'Imbuia',
     card: path + 'imbuia' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'imbuia.jpg'
+}, {
     id: 13,
     nome: 'Ipê-Amarelo',
     card: path + 'ipe-amarelo' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'ipê-amarelo.jpg'
+}, {
     id: 14,
     nome: 'Ipê-Branco',
     card: path + 'ipe-branco' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'ipê-branco.jpg'
+}, {
     id: 15,
     nome: 'Jacarandá-de-Minas',
     card: path + 'jacaranda-de-minas' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'jacaranda-de-minas.png'
+}, {
     id: 16,
-    nome: 'Jacaranda-Paulista',
+    nome: 'Jacarandá-Paulista Jacaranda-Paulista',
     card: path + 'jacaranda-paulista' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'jacaranda-paulista.jpg'
+}, {
     id: 17,
-    nome: 'Manacá-da-Serra',
+    nome: 'Manacá-da-Serra Manaca da Serra Manacá da Serra',
     card: path + 'manaca-da-serra' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'manaca-da-serra.png'
+}, {
     id: 18,
-    nome: 'Pau-Amarelo',
+    nome: 'Pau-Amarelo pau amarelo',
     card: path + 'pau-amarelo' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'pau-amarelo.jpg'
+}, {
     id: 19,
-    nome: 'Pau-Brasil',
+    nome: 'Pau-Brasil pau brasil',
     card: path + 'pau-brasil' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'pau-brasil.jpg'
+}, {
     id: 20,
     nome: 'Quaresmeira',
     card: path + 'quaresmeira' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'quaresmeira.jpg'
+}, {
     id: 21,
     nome: 'Sapuva',
     card: path + 'sapuva' + '.png',
-    imgReal: ''
-},{
+    imgReal: pathArvoreReal + 'sapuva.jpg'
+}, {
     id: 22,
     nome: 'Sibipiruna',
     card: path + 'sibipiruna' + '.png',
-    imgReal: ''
+    imgReal: pathArvoreReal + 'sibipiruna.jpg'
 },
 ]
 
